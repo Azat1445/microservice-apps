17d26583851b01b2f22f9baa4102b54a55d4e8b4
Azat1445 <azatermolaev2@gmail.com>
2026-01-19 22:32:01 +0300

    init commit: add three microservices

---
+++ src/test/java/org/example/microserviceapps/MicroserviceAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.microserviceapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
